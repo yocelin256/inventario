4250e95dd5602340b671c8b0d63b24236f3cc21b
Jocelyn <108595058+yocelin256@users.noreply.github.com>
2025-07-30 12:57:13 -0600

    inventario

---
+++ layout/menu.php
@@ -0,0 +1,208 @@
+<!-- inicio del memu -->
+<div class="sidebar" id="sidebar">
+<div class="sidebar-inner slimscroll">
+<div id="sidebar-menu" class="sidebar-menu">
+<ul>
+<li class="active">
+<a href="index.html"><img src="assets/img/icons/dashboard.svg" alt="img"><span> Panel</span> </a>
+</li>
+<li class="submenu">
+<a href="javascript:void(0);"><img src="assets/img/icons/product.svg" alt="img"><span> Producto</span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="productlist.html">Lista de productos</a></li>
+<li><a href="addproduct.html">Agregar producto</a></li>
+<li><a href="categorylist.html">Lista de Categorias</a></li>
+<li><a href="addcategory.html">Agregar Categoria</a></li>
+<li><a href="subcategorylist.html">Lista de Subcategoria</a></li>
+<li><a href="subaddcategory.html">Agregar Subcategoria</a></li>
+<li><a href="brandlist.html">Lista de marcas</a></li>
+<li><a href="addbrand.html">Agregar marca</a></li>
+<li><a href="importproduct.html">Productos de importacion</a></li>
+<li><a href="barcode.html">Imprimir codigo de barras</a></li>
+</ul>
+</li>
+<li class="submenu">
+<a href="javascript:void(0);"><img src="assets/img/icons/sales1.svg" alt="img"><span>Ventas</span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="saleslist.html">Lista de ventas</a></li>
+<li><a href="pos.html">Puntos de venta</a></li>
+<li><a href="pos.html">Nuevas ventas</a></li>
+<li><a href="salesreturnlists.html">Lista de devoluciones de ventas</a></li>
+<li><a href="createsalesreturns.html">Nueva devolucion de ventas</a></li>
+</ul>
+</li>
+<li class="submenu">
+<a href="javascript:void(0);"><img src="assets/img/icons/purchase1.svg" alt="img"><span> Compras</span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="purchaselist.html">Lista de compras</a></li>
+<li><a href="addpurchase.html">Añadir compra</a></li>
+<li><a href="importpurchase.html">Compra de importacion</a></li>
+</ul>
+</li>
+<li class="submenu">
+<a href="javascript:void(0);"><img src="assets/img/icons/expense1.svg" alt="img"><span> Gastos</span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="expenselist.html">Lista de gastos</a></li>
+<li><a href="createexpense.html">Agregar gastos</a></li>
+<li><a href="expensecategory.html">Categoria de gastos</a></li>
+</ul>
+</li>
+<li class="submenu">
+<a href="javascript:void(0);"><img src="assets/img/icons/quotation1.svg" alt="img"><span> Cotizacion</span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="quotationList.html">Lista de cotizaciones</a></li>
+ <li><a href="addquotation.html">Añadir cotizacion</a></li>
+</ul>
+</li>
+<li class="submenu">
+<a href="javascript:void(0);"><img src="assets/img/icons/transfer1.svg" alt="img"><span> Transferir</span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="transferlist.html">Lista de transferencias</a></li>
+<li><a href="addtransfer.html">Agregar transferencia </a></li>
+<li><a href="importtransfer.html">Transferencia de importacion </a></li>
+</ul>
+</li>
+<li class="submenu">
+<a href="javascript:void(0);"><img src="assets/img/icons/return1.svg" alt="img"><span> Devolver</span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="salesreturnlist.html">Lista de devolucion de ventas</a></li>
+<li><a href="createsalesreturn.html">Agregar devolucion de ventas</a></li>
+<li><a href="purchasereturnlist.html">Lista de devoluciones de compras</a></li>
+<li><a href="createpurchasereturn.html">Agregar devolucion de compra </a></li>
+</ul>
+</li>
+<li class="submenu">
+<a href="javascript:void(0);"><img src="assets/img/icons/users1.svg" alt="img"><span> Clientes</span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="customerlist.html">Lista de clientes</a></li>
+<li><a href="addcustomer.html">Agregar cliente </a></li>
+<li><a href="supplierlist.html">Lista de proveedores</a></li>
+<li><a href="addsupplier.html">Agregar proveedor </a></li>
+<li><a href="userlist.html">Lista de usuarios</a></li>
+<li><a href="adduser.html">Agregar usuario</a></li>
+<li><a href="storelist.html">Lista de tiendas</a></li>
+<li><a href="addstore.html">Agregar tienda</a></li>
+</ul>
+</li> <li class="submenu">
+<a href="javascript:void(0);"><img src="assets/img/icons/places.svg" alt="img"><span> Lugares</span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="newcountry.html">New pais</a></li>
+<li><a href="countrieslist.html">Lista de paises</a></li>
+<li><a href="newstate.html">Nuevo estado </a></li>
+<li><a href="statelist.html">Lista de estados</a></li>
+</ul>
+</li>
+<li>
+<a href="components.html"><i data-feather="layers"></i><span> Componentes</span> </a>
+</li>
+
+<!-- <li class="submenu">
+<a href="javascript:void(0);"><i data-feather="box"></i> <span>Elementos </span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="sweetalerts.html">Sweet Alerts</a></li>
+<li><a href="tooltip.html">Tooltip</a></li>
+<li><a href="popover.html">Popover</a></li>
+<li><a href="ribbon.html">Ribbon</a></li>
+<li><a href="clipboard.html">Clipboard</a></li>
+<li><a href="drag-drop.html">Drag & Drop</a></li>
+<li><a href="rangeslider.html">Range Slider</a></li>
+<li><a href="rating.html">Rating</a></li>
+<li><a href="toastr.html">Toastr</a></li>
+<li><a href="text-editor.html">Text Editor</a></li>
+<li><a href="counter.html">Counter</a></li>
+<li><a href="scrollbar.html">Scrollbar</a></li>
+<li><a href="spinner.html">Spinner</a></li>
+<li><a href="notification.html">Notification</a></li>
+<li><a href="lightbox.html">Lightbox</a></li>
+<li><a href="stickynote.html">Sticky Note</a></li>
+<li><a href="timeline.html">Timeline</a></li>
+<li><a href="form-wizard.html">Form Wizard</a></li>
+</ul>
+</li> -->
+<!-- <li class="submenu">
+<a href="javascript:void(0);"><i data-feather="bar-chart-2"></i> <span> Charts </span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="chart-apex.html">Apex Charts</a></li>
+<li><a href="chart-js.html">Chart Js</a></li>
+<li><a href="chart-morris.html">Morris Charts</a></li>
+<li><a href="chart-flot.html">Flot Charts</a></li>
+<li><a href="chart-peity.html">Peity Charts</a></li>
+</ul>
+</li> -->
+<!-- <li class="submenu">
+<a href="javascript:void(0);"><i data-feather="award"></i><span> Icons </span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="icon-fontawesome.html">Fontawesome Icons</a></li>
+<li><a href="icon-feather.html">Feather Icons</a></li>
+<li><a href="icon-ionic.html">Ionic Icons</a></li>
+<li><a href="icon-material.html">Material Icons</a></li>
+<li><a href="icon-pe7.html">Pe7 Icons</a></li>
+<li><a href="icon-simpleline.html">Simpleline Icons</a></li>
+<li><a href="icon-themify.html">Themify Icons</a></li>
+<li><a href="icon-weather.html">Weather Icons</a></li>
+<li><a href="icon-typicon.html">Typicon Icons</a></li>
+<li><a href="icon-flag.html">Flag Icons</a></li>
+</ul>
+</li> -->
+<!-- <li class="submenu">
+<a href="javascript:void(0);"><i data-feather="columns"></i> <span> Forms </span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="form-basic-inputs.html">Basic Inputs </a></li>
+<li><a href="form-input-groups.html">Input Groups </a></li>
+<li><a href="form-horizontal.html">Horizontal Form </a></li>
+<li><a href="form-vertical.html"> Vertical Form </a></li>
+<li><a href="form-mask.html">Form Mask </a></li>
+<li><a href="form-validation.html">Form Validation </a></li>
+<li><a href="form-select2.html">Form Select2 </a></li>
+<li><a href="form-fileupload.html">File Upload </a></li>
+</ul>
+</li> -->
+<!-- <li class="submenu">
+<a href="javascript:void(0);"><i data-feather="layout"></i> <span> Table </span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="tables-basic.html">Basic Tables </a></li>
+<li><a href="data-tables.html">Data Table </a></li>
+</ul>
+</li> -->
+<!-- <li class="submenu">
+<a href="javascript:void(0);"><img src="assets/img/icons/product.svg" alt="img"><span> Application</span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="chat.html">Chat</a></li>
+<li><a href="calendar.html">Calendar</a></li>
+<li><a href="email.html">Email</a></li>
+</ul>
+</li> -->
+<li class="submenu">
+<a href="javascript:void(0);"><img src="assets/img/icons/time.svg" alt="img"><span> Informe</span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="purchaseorderreport.html">Informe de ordenes de compra</a></li>
+<li><a href="inventoryreport.html">Informde inventario</a></li>
+<li><a href="salesreport.html">Informe de ventas</a></li>
+<li><a href="invoicereport.html">Informe de facturas</a></li>
+<li><a href="purchasereport.html">Informe de compra</a></li>
+<li><a href="supplierreport.html">Informe de proveedores</a></li>
+<li><a href="customerreport.html">Informe del cliente</a></li>
+</ul>
+</li>
+<li class="submenu">
+<a href="javascript:void(0);"><img src="assets/img/icons/users1.svg" alt="img"><span> Usuarios</span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="newuser.html">Nuevo usuario </a></li>
+<li><a href="userlists.html">Lista de usuarios</a></li>
+</ul>
+</li>
+<!-- <li class="submenu">
+<a href="javascript:void(0);"><img src="assets/img/icons/settings.svg" alt="img"><span> Ajustes</span> <span class="menu-arrow"></span></a>
+<ul>
+<li><a href="generalsettings.html">Configuracion general</a></li>
+ <li><a href="paymentsettings.html">Payment Settings</a></li>
+<li><a href="currencysettings.html">Currency Settings</a></li>
+<li><a href="grouppermissions.html">Group Permissions</a></li>
+<li><a href="taxrates.html">Tax Rates</a></li>
+</ul>
+</li> -->
+</ul>
+</div>
+</div>
+</div>
+<!-- fin del menu -->
--- sql/inventario.sql
+++ sql/inventario.sql
@@ -1,17 +1,22 @@
-CREATE DATABASE inventario_club;
+CREATE DATABASE IF NOT EXISTS inventario_club;
 USE inventario_club;
 
 CREATE TABLE usuarios (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(100),
   usuario VARCHAR(50) UNIQUE,
+  email VARCHAR(100) UNIQUE,
   contrasena VARCHAR(255),
-  rol ENUM('admin','usuario') DEFAULT 'usuario'
+  rol ENUM('admin','usuario') DEFAULT 'usuario',
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE categorias (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(100) UNIQUE
+  nombre VARCHAR(100) UNIQUE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE articulos (
@@ -24,7 +29,9 @@ CREATE TABLE articulos (
   fecha_ingreso DATE,
   imagen VARCHAR(255),
   categoria_id INT,
-  FOREIGN KEY(categoria_id) REFERENCES categorias(id)
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (categoria_id) REFERENCES categorias(id) ON DELETE SET NULL
 );
 
 CREATE TABLE movimientos (
@@ -34,6 +41,6 @@ CREATE TABLE movimientos (
   cantidad INT,
   fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   usuario_id INT,
-  FOREIGN KEY(articulo_id) REFERENCES articulos(id),
-  FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
+  FOREIGN KEY (articulo_id) REFERENCES articulos(id) ON DELETE CASCADE,
+  FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE SET NULL
 );
